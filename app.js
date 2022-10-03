0444c4e348f9c26c80c7d877b792f2ce49682881
sakariye10 <sakariyesaeed51@gmail.com>
2022-10-04 00:32:41 +0300

    waa js

---
+++ app.js
@@ -0,0 +1,30 @@
+// const companayname ="telelsom";
+// console.log(companayname.toUpperCase())
+const fullname = "tiigsi";
+const text ="tiigsi";
+console.log( text.charAt(0).toUpperCase() + text.slice(2))
+
+
+
+
+// const fullname ="    tiigsi technology    ";
+// console.log(fullname.toUpperCase().trim())
+// console.log(fullname.indexOf("i"))
+
+
+// const One = "cabilahi moh";
+// console.log(One[0][1].toUpperCase() . One.slice(1).slice(8))
+   /// replace
+   // const color="casaan web casaan"
+   // console.log(color.replace('s','d'))
+///repeat
+// const wllo ="waan xanaaqsanaay"
+// console.log('waan ku jecelahay '.repeat(100))
+//sidee laba magca la isku hor dhigaa
+// const firtname="ali";
+// const lastname="siyaad"
+// const fullname= firtname + ' ' + lastname;
+// const fullname= `${firtname} ${lastname}`;
+
+// console.log(fullname)
+    
